6664238a03318798c84f4ea83db864e414474f56
Yudai Hara <yudai.h.1994@gmail.com>
2016-04-08 16:46:02 +0900

    Playerクラス作成。Scene1で作動。

--- MasatoScene1.cs
+++ MasatoScene1.cs
@@ -8,14 +8,18 @@ namespace HackTheWorld
 {
     class MasatoScene1 : Scene
     {
+        //このSceneで使用する変数
         Image _img;
-        public override void Cleanup()
+        Player player;
+
+        public override void Cleanup()//終了時処理
         {
         }
 
-        public override void Startup()
+        public override void Startup()//初期化処理
         {
             _img = Image.FromFile(@"image\masato1.jpg");
+            player = new Player(new Vector(100, 100));
         }
 
         public override void Update()
@@ -24,9 +28,20 @@ namespace HackTheWorld
             {
                 Scene.Pop();
             }
-            
+            //ここに作成
+            // 計算とか
+
+            player.move();
+
+
             GraphicsContext.Clear(Color.White);
-            GraphicsContext.DrawImage(_img, 0, 0);
+            //GraphicsContext.DrawImage(_img, 0, 0);
+
+            //ここに作成
+            // 描画のみ
+
+            player.draw(_img);
+
 
         }
     }
