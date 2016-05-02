9fa93c9f660a33e99e3716e68951249dc0e84433
Yudai Hara <yudai.h.1994@gmail.com>
2016-05-02 17:33:32 +0900

    MasatoScene1内のコードを整頓

--- MasatoScene1.cs
+++ MasatoScene1.cs
@@ -8,24 +8,30 @@ namespace HackTheWorld
 {
     class MasatoScene1 : Scene
     {
-        Image _img;
+        // ゲーム画面外の変数の定義
         private readonly MenuItem _backButton = new MenuItem(Image.FromFile(@"image\back.png"));
         private readonly MenuItem _resetButton = new MenuItem(Image.FromFile(@"image\reset.jpg"));
+
+        // ゲーム内変数宣言
+        Image _img;
         Player _player;
         List<GameObject> _blocks;
+
         public override void Cleanup()
         {
         }
 
         public override void Startup()
         {
-            _img = Image.FromFile(@"image\masato1.jpg");
-
+            // ゲーム画面外初期化
             _backButton.Size = new Vector(50, 50);
             _backButton.Position = new Vector(25, 600);
             _resetButton.Size = new Vector(100,50);
             _resetButton.Position = new Vector(100,600);
+            
+            // ゲーム内初期化
             // playerの初期化
+            _img = Image.FromFile(@"image\masato1.jpg");
             _player = new Player(_img);
 
             // ブロックの初期化
@@ -44,10 +50,18 @@ namespace HackTheWorld
 
         public override void Update(float dt)
         {
+            // ゲーム外処理
             if (Input.Sp2.Pushed)
             {
                 Scene.Pop();
             }
+            if (_backButton.Clicked) Scene.Pop();
+            if (_resetButton.Clicked) Startup();
+            _backButton.Draw();
+            _resetButton.Draw();
+
+
+            // ゲーム内処理
 
             _player.Update(dt);
 
@@ -60,19 +74,21 @@ namespace HackTheWorld
                 _player.Adjust(block);
             }
 
-            GraphicsContext.Clear(Color.White);
+            // 画面のクリア
+            ScreenClear();
+
+            // 描画
             _player.Draw();
             foreach (var block in _blocks)
             {
                 block.Draw();
             }
 
+        }
 
-
-            if (_backButton.Clicked) Scene.Pop();
-            if (_resetButton.Clicked) Startup(); ;
-            _backButton.Draw();
-            _resetButton.Draw(); ;
+        private void ScreenClear()
+        {
+            GraphicsContext.Clear(Color.White);
         }
     }
 }
