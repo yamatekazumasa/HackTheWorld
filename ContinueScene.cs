0376564160ec371cf4ea677214d95da0a7246d36
yamada <0123456789@toki.waseda.jp>
2016-05-05 01:27:38 +0900

    #56

--- ContinueScene.cs
+++ ContinueScene.cs
@@ -11,25 +11,47 @@ namespace HackTheWorld
 {
     class ContinueScene : Scene
     {
+        //画像を読み込む
+        Bitmap bmp = new Bitmap(@"image\gameover.bmp");
+
         private readonly MenuItem _continueButton = new MenuItem(Image.FromFile(@"image\continue.bmp"), Image.FromFile(@"image\continue1.bmp"));
         private readonly MenuItem _closeButton = new MenuItem(Image.FromFile(@"image\close1.bmp"), Image.FromFile(@"image\close.bmp"));
         List<MenuItem> menuItem = new List<MenuItem>();
-
-        //コントロールの外観を描画するBitmapの作成
-        Bitmap bmp = new Bitmap(ScreenWidth, ScreenHeight);
         public override void Cleanup()
         {
         }
         public override void Startup()
         {
             _continueButton.Size = new Vector(400, 100);
-            _continueButton.Position = new Vector(400, 200);
+            _continueButton.Position = new Vector(800, 200);
             _closeButton.Size = new Vector(400, 100);
-            _closeButton.Position = new Vector(400, 400);
+            _closeButton.Position = new Vector(800, 300);
             menuItem.Add(_continueButton); menuItem.Add(_closeButton);
+         //   System.Threading.Thread.Sleep(1000);
+
+
+
         }
         public override void Update(float dt)
         {
+            //    //ColorMatrixオブジェクトの作成
+            //    System.Drawing.Imaging.ColorMatrix cm = new System.Drawing.Imaging.ColorMatrix();
+            //    //ColorMatrixの行列の値を変更して、アルファ値が0.5に変更されるようにする
+            //    cm.Matrix00 = 1;
+            //    cm.Matrix11 = 1;
+            //    cm.Matrix22 = 1;
+            //    cm.Matrix33 = 0.5F;
+            //    cm.Matrix44 = 1;
+            //    //ImageAttributesオブジェクトの作成
+            //    System.Drawing.Imaging.ImageAttributes ia = new System.Drawing.Imaging.ImageAttributes();
+            //    //ColorMatrixを設定する
+            //    ia.SetColorMatrix(cm);
+            //    //ImageAttributesを使用して画像を描画
+
+            //背景を透明にする
+            bmp.MakeTransparent();
+            GraphicsContext.DrawImage(bmp,  0, 0);
+
             foreach (var button in menuItem)
             {
                 button.IsSelected = false;
