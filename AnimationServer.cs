0164f1eaa9e6975cc08233950e90f58e37ff5211
takasumiu <takasumiu@gmail.com>
2016-05-28 17:20:04 +0900

    Add: アニメーション途中まで

---
+++ AnimationServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackTheWorld
+{
+    public class AnimationServer
+    {
+        public static AnimationServer Server { get; } = new AnimationServer();
+
+        private List<IAnimatable> _animations;
+
+        public void Advance(float dt)
+        {
+            _animations.ForEach(a => a.Advance(dt));
+        }
+
+        public void Add(IAnimatable a)
+        {
+            _animations.Add(a);
+            a.Start(this);
+        }
+
+        public void Remove(IAnimatable anim)
+        {
+            _animations.Remove(anim);
+            anim.Stop();
+        }
+
+        public void RemoveAll()
+        {
+            _animations.ForEach(a => a.Stop());
+            _animations = new List<IAnimatable>();
+        }
+
+    }
+}
---
+++ IAnimatable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackTheWorld
+{
+    public delegate void OnComplete(IAnimatable anim);
+
+    public interface IAnimatable
+    {
+        float ElapsedTime { get; set; }
+        AnimationServer AnimationServer { get; set; }
+        bool IsPlaying { get; set; }
+        OnComplete OnComplete { get; set; }
+        void Advance(float dt);
+
+        void Start(AnimationServer server);
+        void Stop();
+    }
+
+    public partial class Extensions
+    {
+        public static void Start(this IAnimatable self, AnimationServer server)
+        {
+            self.AnimationServer = server;
+            self.ElapsedTime = 0.0f;
+            self.IsPlaying = true;
+        }
+
+        public static void Stop(this IAnimatable self)
+        {
+            self.OnComplete?.Invoke(self);
+            self.IsPlaying = false;
+        }
+
+        public static void Rewind(this IAnimatable self)
+        {
+            self.ElapsedTime = 0.0f;
+        }
+
+        public static void Update(this IAnimatable self, float dt)
+        {
+            self.ElapsedTime += dt;
+            self.Advance(dt);
+        }
+
+    }
+}
