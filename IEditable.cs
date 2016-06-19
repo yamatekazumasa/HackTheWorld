7ca07bbefd688fc3cf9514212c4c5b2edf110b8e
Kohei Yokouchi <Kohei Yokouchi>
2016-06-19 12:03:35 +0900

    触れたときに動作するような設定を追加中(未完成)

--- IEditable.cs
+++ IEditable.cs
@@ -102,7 +102,7 @@ namespace HackTheWorld
             self.Codebox.Update();
         }
 
-        public static void Compile(this IEditable self)
+        public static void Compile(this IEditable self, Stage s)
         {
             string str = self.Codebox.GetString();
             // ここにstring型をProcess型に変換する処理を書く。
@@ -113,41 +113,93 @@ namespace HackTheWorld
 
             // self.SetProcesses(new Process[] {});
 
+
             //以下のリストの中身("move, x, y")を小集合とする
-            var array = new string[] { "size,50,30","wait,3","move,1,1,2" };
+            var array = new List<string> { "size,1,1", "wait,1", "touch,move,1,1,2" };
 
 
             //各小集合に対して、以下の分割処理を行う。
-            foreach (var s in array)
+            foreach (var elements in array)
             {
                 //小集合を要素に分割して、要素数1-4程度の配列を作成
-                string[] tmp = s.Split(',');
+                string[] tmp = elements.Split(',');
 
                 //具体的に配列の数字 tmp[1], tmp[2]を与えることで、正確にインデックスの範囲内で処理を完結させる
                 //ArgumentOutOfRangeExceptionエラーを回避
-                switch (tmp[0])
-                {
-                    case "size":
-                        self.AddProcess(new Process((obj, dt) => { obj.Size = new Vector(float.Parse(tmp[1]), float.Parse(tmp[2])); }));
-                        break;
 
-                    case "wait":
-                        self.AddProcess(new Process((obj, dt) => { obj.Position += new Vector(0,0); }, float.Parse(tmp[1])));
-                        break;
-
-                    case "move":
-                        self.AddProcess(new Process((obj, dt) => { obj.Position += new Vector(float.Parse(tmp[1]), float.Parse(tmp[2])); }, float.Parse(tmp[3])));
-                        break;
-
-                    default:
-                        break;
-                }
             }
-            
-
         }
 
+        //Processに関する基本関数
+        public static void BasicAction(this IEditable self, string[] tmp)
+        {
+            switch (tmp[0])
+            {
+                //大きさ
+                case "size":
+                    self.AddProcess(new Process((obj, dt) => { obj.W = CellSize * float.Parse(tmp[1]); }));
+                    self.AddProcess(new Process((obj, dt) => { obj.H = CellSize * float.Parse(tmp[2]); }));
+                    break;
 
+                //待機
+                case "wait":
+                    self.AddProcess(new Process((obj, dt) => { obj.VX = 0.0f; }));
+                    self.AddProcess(new Process((obj, dt) => { obj.VY = 0.0f; }));
+                    self.AddProcess(new Process((obj, dt) => { obj.Move(dt); }, float.Parse(tmp[1])));
+                    break;
+
+                //移動
+                case "move":
+                    self.AddProcess(new Process((obj, dt) => { obj.VX = CellSize * float.Parse(tmp[1]); }));
+                    self.AddProcess(new Process((obj, dt) => { obj.VY = CellSize * float.Parse(tmp[2]); }));
+                    self.AddProcess(new Process((obj, dt) => { obj.Move(dt); }, float.Parse(tmp[3])));
+                    break;
+
+
+                default:
+                    break;
+            }
+        }
+
+        //様々な条件があるときの動作
+        public static void ConditionalAction(this IEditable self, string[] tmp)
+        {
+            //条件達成時に行われるべき配列を作る
+
+
+            switch (tmp[0])
+            {
+                //オブジェクトに当たった時の判定
+                case "touch":
+
+                    self.AddProcess(new Process((obj, dt) =>
+                    {
+
+                    }));
+                    break;
+                //オブジェクトに乗った時の判定
+                case "ontop":
+
+                    //オブジェクト上部に判定エリアをつける
+                    var judge_area = new GameObject();
+                    judge_area.MidX = self.MidX;
+                    judge_area.MidY = self.Y;
+                    judge_area.W = self.W;
+                    judge_area.H = CellSize / 8.0f;
+
+                    //判定エリアにいるかどうかで処理するかを決める
+                    if (self.CollidesWith(judge_area))
+                    {
+
+                    }
+                    break;
+
+
+                default:
+                    break;
+            }
+
+        }
 
 
     }
