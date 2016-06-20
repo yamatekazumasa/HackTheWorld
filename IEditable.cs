0d10d16bb4b9432255facb9e0581f237143b3160
Kohei Yokouchi <Kohei Yokouchi>
2016-06-20 14:23:52 +0900

    Process作るときにStageのPlayer情報を参照できるように整形

--- IEditable.cs
+++ IEditable.cs
@@ -102,7 +102,7 @@ namespace HackTheWorld
             self.Codebox.Update();
         }
 
-        public static void Compile(this IEditable self)
+        public static void Compile(this IEditable self, Stage s)
         {
             string str = self.Codebox.GetString();
             // ここにstring型をProcess型に変換する処理を書く。
@@ -124,18 +124,55 @@ namespace HackTheWorld
                 //小集合を要素に分割して、要素数1-4程度の配列を作成
                 string[] tmp = elements.Split(',');
 
+
+                #region 特殊な関数の場合の処理
                 //基本関数でなければ特殊処理
                 if (tmp[0] != "size" && tmp[0] != "wait" && tmp[0] != "move")
                 {
-                    ConditionalAction(self,tmp);
+                    //条件達成時に行われるべき配列を作る
+                    List<string> strtmp = new List<string>();
+                    strtmp.AddRange(tmp);
+                    strtmp.RemoveAt(0);
+
+                    switch (tmp[0])
+                    {
+                        //オブジェクトに当たった時の判定
+                        case "touch":
+
+                            self.AddProcess(new Process((obj, dt) =>
+                            {
+
+                            }));
+                            break;
+                        //オブジェクトに乗った時の判定
+                        case "ontop":
+
+                            //オブジェクト上部に判定エリアをつける
+                            var judge_area = new GameObject();
+                            judge_area.MidX = self.MidX;
+                            judge_area.MidY = self.Y;
+                            judge_area.W = self.W;
+                            judge_area.H = CellSize / 8.0f;
+
+                            //判定エリアにいるかどうかで処理するかを決める
+                            if (self.CollidesWith(judge_area))
+                            {
+
+                            }
+                            break;
+
+
+                        default:
+                            break;
+                    }
                 }
+                #endregion
 
                 BasicAction(self, tmp);
-
             }
         }
 
-        //Processに関する基本関数
+        //Processに関する基本動作関数
         public static void BasicAction(this IEditable self, string[] tmp)
         {
             switch (tmp[0])
@@ -163,48 +200,8 @@ namespace HackTheWorld
                 default:
                     break;
             }
-        }
-
-        //様々な条件があるときの動作
-        public static void ConditionalAction(this IEditable self, string[] tmp)
-        {
-            //条件達成時に行われるべき配列を作る
-
-
-            switch (tmp[0])
-            {
-                //オブジェクトに当たった時の判定
-                case "touch":
-
-                    self.AddProcess(new Process((obj, dt) =>
-                    {
-
-                    }));
-                    break;
-                //オブジェクトに乗った時の判定
-                case "ontop":
-
-                    //オブジェクト上部に判定エリアをつける
-                    var judge_area = new GameObject();
-                    judge_area.MidX = self.MidX;
-                    judge_area.MidY = self.Y;
-                    judge_area.W = self.W;
-                    judge_area.H = CellSize / 8.0f;
-
-                    //判定エリアにいるかどうかで処理するかを決める
-                    if (self.CollidesWith(judge_area))
-                    {
-
-                    }
-                    break;
-
-
-                default:
-                    break;
-            }
 
         }
-
 
     }
 
