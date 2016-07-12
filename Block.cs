cd1adc94f8dfbe5aab966cd309dfd34f73647b06
y-takashina <takashina2051@gmail.com>
2016-07-12 13:41:48 +0900

    IDrawable で画像を描画できるように。

--- Block.cs
+++ Block.cs
@@ -7,7 +7,7 @@ namespace HackTheWorld
     /// ブロック。
     /// サイズは CellSize で固定。
     /// </summary>
-    public class Block : GameObject
+    public class Block : GameObject, IDrawable
     {
         public Block(float x, float y) : base(x, y) { }
 
@@ -15,10 +15,20 @@ namespace HackTheWorld
 
         public Block(float x, float y, float vx, float vy, float w, float h) : base(x, y, vx, vy, w, h) { }
 
+        public Image Image { get; set; }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            Image = Image.FromFile(@"image\masato1.jpg");
+        }
+
         public override void Draw()
         {
-            GraphicsContext.FillRectangle(Brushes.Brown, X, Y, Width, Height);
-            GraphicsContext.DrawRectangle(Pens.Black   , X, Y, Width, Height);
+            ((IDrawable) this).Draw();
+//            GraphicsContext.FillRectangle(Brushes.Brown, X, Y, Width, Height);
+//            GraphicsContext.DrawRectangle(Pens.Black   , X, Y, Width, Height);
         }
+
     }
 }
---
+++ IDrawable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HackTheWorld.Constants;
+
+namespace HackTheWorld
+{
+    public interface IDrawable
+    {
+        float X { get; set; }
+        float Y { get; set; }
+        float W { get; set; }
+        float H { get; set; }
+        Image Image { get; set; }
+    }
+
+    public static partial class Extensions
+    {
+        public static void Draw(this IDrawable self)
+        {
+            GraphicsContext.DrawImage(self.Image, self.X, self.Y, self.W, self.H);
+        }
+    }
+}
+
+
